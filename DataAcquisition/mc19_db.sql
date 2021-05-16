ef6b6a99bafe4f326ece2bf506f3545f2f339d30
Fatoumata921 <81049812+Fatoumata921@users.noreply.github.com>
2021-05-16 16:44:43 +0000

    mise a jour bd

---
+++ DataAcquisition/mc19_db.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS `Communiques`;
+DROP TABLE IF EXISTS `Localite`;
+
+CREATE TABLE Localite(
+    nom_Localite VARCHAR(50) ,
+    nombre_cas int(10)  
+) ;
+
+
+CREATE TABLE Communiques (
+  id_communique int(5) PRIMARY KEY,
+  date_communique varchar(50) DEFAULT 'NULL',
+  nb_tests int(5) NOT NULL DEFAULT 0,
+  nb_cas int(5) NOT NULL DEFAULT 0,
+  nb_cas_contact int(5) NOT NULL DEFAULT 0,
+  nb_cas_communautaire int(5) NOT NULL DEFAULT 0,
+  nb_gueris int(5) NOT NULL DEFAULT 0,
+  nb_deces int(5) NOT NULL DEFAULT 0,
+  date_extraction varchar(10)  DEFAULT NULL
+) ;
+
