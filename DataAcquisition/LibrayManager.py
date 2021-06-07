bc4101d43c5c1cce16bb4653afa434d5ea7f8c83
sedame <80451749+sedame@users.noreply.github.com>
2021-06-07 16:43:08 +0100

    Library managing

---
+++ DataAcquisition/LibrayManager.py
@@ -0,0 +1,46 @@
+
+from bs4 import BeautifulSoup as bs
+from bs4 import *
+import requests
+import os
+from textparser import *
+import wget
+
+import tweepy 
+import webbrowser
+import pandas as pd
+
+import csv
+from getpass import getpass
+from time import sleep
+from selenium.webdriver.common.keys import Keys
+#from selenium.webdriver import Chrome
+from selenium.webdriver import Firefox
+from msedge.selenium_tools import Edge, EdgeOptions
+from selenium.common.exceptions import NoSuchElementException
+import webbrowser
+
+
+import os
+import sys
+import wget
+import urllib.request
+import urllib.parse
+from urllib.request import urlopen
+from bs4 import *
+from bs4 import BeautifulSoup
+import requests
+import parse
+
+from json import*
+import json
+
+from mysql import*
+import pymysql
+
+
+
+
+
+def installLib():
+	pass
